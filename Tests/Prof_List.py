1d24d2675c99ed9c0a50f2790b63015f50fdcebc
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-01-20 12:48:21 +0500

    test_proff

--- Tests/Prof_List.py
+++ Tests/Prof_List.py
@@ -2,16 +2,16 @@ import json
 def diction(items):
     id_list = []
     name_list = []
-    for i in items():
+    for i in items:
         for id in i['specializations']:
             id_list.append(id["id"])
 
-    for i in items():
+    for i in items:
         for id in i['specializations']:
             name_list.append(id["name"])
 
-    diction = dict(zip(name_list, id_list))
-    return diction
+    dictions = dict(zip(name_list, id_list))
+    return dictions
 
 with open("profession", 'r', encoding='utf-8') as t:
     text = t.read()
