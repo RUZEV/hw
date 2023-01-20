c84ecef0f27a702673b7bb3e38a8c4f84a13ed04
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-01-20 12:43:02 +0500

    list_of_prof

---
+++ Tests/Prof_List.py
@@ -0,0 +1,23 @@
+import json
+def diction(items):
+    id_list = []
+    name_list = []
+    for i in items():
+        for id in i['specializations']:
+            id_list.append(id["id"])
+
+    for i in items():
+        for id in i['specializations']:
+            name_list.append(id["name"])
+
+    diction = dict(zip(name_list, id_list))
+    return diction
+
+with open("profession", 'r', encoding='utf-8') as t:
+    text = t.read()
+    items = json.loads(text)
+    diction(items)
+
+
+
+
