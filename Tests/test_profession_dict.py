03c791653ee4113c4bbaa84d1301b81d00e63fec
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-01-21 14:54:49 +0500

    Test

--- Tests/test_profession_dict.py
+++ Tests/test_profession_dict.py
@@ -12,4 +12,12 @@ class TestPars(unittest.TestCase):
     def test_format(self):
         self.assertDictEqual(Tests)
 
+class json_search_test(unittest.TestCase):
+    def test_search_found(self):
+        '''key should be found, return list should not be empty'''
+        self.assertTrue([]!=json_search(key1, data))
+
+    def test_search_not_found(self):
+        '''key should not be found, should return an empty list'''
+        self.assertTrue([] == json_search(key2, data))
 
