6096ea8d5fdd71f54b24b0be54637e02a5dc9509
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-01-21 14:42:06 +0500

    Test

--- Tests/test_profession_dict.py
+++ Tests/test_profession_dict.py
@@ -1,4 +1,15 @@
 import unittest
+import Prof_List as PR
+
+class MyClass(object):
+    def __init__(self, foo):
+        if foo != 1:
+            raise ValueError('foo is not equal to 1!')
+class MyClass2(object):
+    def __init__(self):
+        pass
 class TestPars(unittest.TestCase):
-    def test_dict(self):
-        self.parser = 3123
+    def test_format(self):
+        self.assertDictEqual(Tests)
+
+
