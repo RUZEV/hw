953260027e234aa075f24aba0623e2cb29eab866
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-01-24 11:10:37 +0500

    test

--- Tests/test_list.py
+++ Tests/test_list.py
@@ -3,6 +3,14 @@ import list_id_for_test as lt
 
 
 class json_search_test(unittest.TestCase):
+    def test_list_is_list(self):
+        self.assertIsInstance(lt.dictor, list)
+
     def test_id_is_float(self):
         for i in range(0, len(lt.dictor)-1):
-            self.assertEqual(float(lt.dictor[i]), float)
+            self.assertIs(float(lt.dictor[i]), float)
+
+    def test_is_a_list(self):
+        list_float = [float(i) for i in lt.fetch(lt.items, lt.dictor)]
+        self.assertIs(list_float[1], float)
+
