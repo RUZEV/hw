9552f27d3de99bb0f69da2e95b19fb7a2e066354
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-01-26 12:00:32 +0500

    test

--- Tests/list_id_for_test.py
+++ Tests/list_id_for_test.py
@@ -3,10 +3,15 @@ import json
 def fetch(dicionary, dictions):
     for i in dicionary:
         for id in i['specializations']:
-            dictions.append(id["id"])
+            try:
+                if float(id["id"]) is not False:
+                    dictions.append(id["id"])
+            except ValueError:
+                print('Найдено не числовое значение: ' + id['id'])
     return dictions
 
-with open('profession', 'r', encoding='utf-8') as t:
+
+with open('profession1', 'r', encoding='utf-8') as t:
     text = t.read()
     items = json.loads(text)
 
